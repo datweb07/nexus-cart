6c82fe0996b83462b1e34d3e7b8a1bfdad2bfde4
datweb07 <dat82770@gmail.com>
2026-05-06 21:57:59 +0700

    feat: implement sidebar view component

---
+++ Repository/Components/BrandViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NexusCart.Repository.Components
+{
+    public class BrandViewComponent : ViewComponent
+    {
+        private readonly DBContext _context;
+        public BrandViewComponent(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var brands = await _context.Brands.ToListAsync();
+            return View(brands);
+        }
+    }
+}
