b9fe108720a0fff089c6db8c9024ddd9881bdc1b
Đạt Trương <dat82770@gmail.com>
2026-05-06 21:58:35 +0700

    feat: implement sidebar view component (#6)

---
+++ Repository/Components/CategoryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NexusCart.Repository.Components
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly DBContext _context;
+        public CategoryViewComponent(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
