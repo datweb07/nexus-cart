1eea3e21040be84e4f4188d33c28c39f3fade141
datweb07 <dat82770@gmail.com>
2026-05-06 16:09:42 +0700

    feat: configure DbContext and database connection

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,6 +1,17 @@
-﻿namespace NexusCart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusCart.Models
 {
     public class CategoryModel
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        public string ?Name { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+        public string ?Description { get; set; }
+        [Required]
+        public string ?Slug { get; set; }
+        public int Status { get; set; }
     }
 }
---
+++ Repository/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NexusCart.Repository
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.BrandModel> Brands { get; set; } 
+        public DbSet<Models.CategoryModel> Categories { get; set; }
+        public DbSet<Models.ProductModel> Products { get; set; }
+    }
+}
