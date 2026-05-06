2ed651c6fcff7bd67e7906a356e00adffcd81446
Đạt Trương <dat82770@gmail.com>
2026-05-06 16:10:21 +0700

    feat: configure DbContext and database connection (#3)

--- Models/BrandModel.cs
+++ Models/BrandModel.cs
@@ -1,6 +1,16 @@
-﻿namespace NexusCart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusCart.Models
 {
     public class BrandModel
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        public string ?Name { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+        public string ?Description { get; set; }
+        public string ?Slug { get; set; }
+        public int Status { get; set; }
     }
 }
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,6 +1,21 @@
-﻿namespace NexusCart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusCart.Models
 {
     public class ProductModel
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        public string ?Name { get; set; }
+        public string ?Slug { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+        public string ?Description { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Price must be at least 4 characters long.")]
+        public decimal Price { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryModel ?Category { get; set; }
+        public BrandModel ?Brand { get; set; }
     }
 }
