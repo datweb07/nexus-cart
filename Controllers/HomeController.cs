da097c9275afcd5c99f08370d032aecdd5e3769d
datweb07 <dat82770@gmail.com>
2026-05-06 22:25:25 +0700

    feat: display product list on UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NexusCart.Models;
+using NexusCart.Repository;
 using System.Diagnostics;
 
 namespace NexusCart.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DBContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(DBContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.Include("Brand").Include("Category").ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
