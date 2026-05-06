3c73054691ba65f0392ca752b533fb6897812a08
Đạt Trương <dat82770@gmail.com>
2026-05-06 22:51:44 +0700

    feat: filter and display products by category (#9)

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NexusCart.Models;
+using NexusCart.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace NexusCart.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly DBContext _context;
+        public CategoryController(DBContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string slug = "")
+        {
+            CategoryModel categoryModel = _context.Categories.Where(c => c.Slug == slug).FirstOrDefault();
+
+            if (categoryModel == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var productByCategory = _context.Products.Where(p => p.CategoryId == categoryModel.Id);
+            return View(await productByCategory.OrderByDescending(p => p.Id).ToListAsync());
         }
     }
 }
