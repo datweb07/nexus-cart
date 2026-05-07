7bcab49f1f13b21d1c0a4cdedfa784061a615c4d
datweb07 <dat82770@gmail.com>
2026-05-07 17:03:29 +0700

    feat: implement product detail page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NexusCart.Models;
+using NexusCart.Repository;
 
 namespace NexusCart.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DBContext _context;
+        public ProductController(DBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public async Task<IActionResult> Details(int id = 0)
         {
-            return View();
+            if (id == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var productById = _context.Products.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(productById);
         }
     }
 }
