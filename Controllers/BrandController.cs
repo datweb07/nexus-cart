08f8f022fd30de09309cb5db6ca7777c7198bee4
datweb07 <dat82770@gmail.com>
2026-05-07 16:03:45 +0700

    feat: filter and display products by brand

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NexusCart.Models;
+using NexusCart.Repository;
+
+namespace NexusCart.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly DBContext _context;
+        public BrandController(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string slug = "")
+        {
+            BrandModel brandModel = _context.Brands.Where(b => b.Slug == slug).FirstOrDefault();
+
+            if (brandModel == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var productByBrand = _context.Products.Where(p => p.BrandId == brandModel.Id);
+
+            return View(await productByBrand.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
