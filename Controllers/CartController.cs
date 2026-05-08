ded9c8dda759c2f4e97767dbd08cc20fb2a8f25d
Đạt Trương <dat82770@gmail.com>
2026-05-08 17:09:20 +0700

    feat: add product to shopping cart (#16)

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -29,5 +29,33 @@ namespace NexusCart.Controllers
         {
             return View("~/Views/Checkout/Index.cshtml");
         }
+
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            ProductModel product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("CartItems") ?? new List<CartItemModel>();
+
+            CartItemModel cartItem = cartItems.FirstOrDefault(item => item.ProductId == id);
+
+            if (cartItem != null)
+            {
+                cartItem.Quantity++;
+            }
+            else
+            {
+                cartItems.Add(new CartItemModel(product) { Quantity = 1 });
+            }
+
+            HttpContext.Session.SetJson("CartItems", cartItems);
+            //return RedirectToAction("Index");
+
+            return Redirect(Request.Headers["Referer"].ToString());     //quay lại trang trước đó
+        }
     }
 }
