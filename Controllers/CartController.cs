55703a1c4c8f19a4c0626b280f35d58850233a44
datweb07 <dat82770@gmail.com>
2026-05-08 16:44:02 +0700

    feat: implement shopping cart list page

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NexusCart.Models;
+using NexusCart.Models.ViewModels;
+using NexusCart.Repository;
 
 namespace NexusCart.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DBContext _context;
+
+        public CartController(DBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("CartItems") ?? new List<CartItemModel>();
+            CartItemViewModel cartItemViewModel = new()
+            {
+                cartItemModels = cartItems,
+                GrandTotal = cartItems.Sum(item => item.TotalPrice)
+            };
+            return View(cartItemViewModel);
         }
 
         public IActionResult Checkout()
