f47cfb21f8395c2bf944019f5f8e753e89b61b2c
Đạt Trương <dat82770@gmail.com>
2026-05-10 11:14:05 +0700

    feat: add validation for uploaded files (#22)

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NexusCart.Models;
 using NexusCart.Repository;
 
 namespace NexusCart.Areas.Admin.Controllers
@@ -18,11 +19,45 @@ namespace NexusCart.Areas.Admin.Controllers
             return View(await _context.Products.OrderByDescending(p => p.Id).Include(p => p.Category).ToListAsync());
         }
 
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
             ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
             ViewBag.Brands = new SelectList(await _context.Brands.ToListAsync(), "Id", "Name");
+
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductModel productModel)
+        {
+            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name", productModel.CategoryId);
+            ViewBag.Brands = new SelectList(await _context.Brands.ToListAsync(), "Id", "Name", productModel.BrandId);
+            return View(productModel);
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(productModel);
+            //}
+
+            //_context.Products.Add(productModel);
+            //await _context.SaveChangesAsync();
+
+            //TempData["success"] = "Product created successfully";
+
+            //return RedirectToAction(nameof(Index));
+        }
+
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var product = await _context.Products.FindAsync(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name", product.CategoryId);
+        //    ViewBag.Brands = new SelectList(await _context.Brands.ToListAsync(), "Id", "Name", product.BrandId);
+        //    return View(product);
+        //}
     }
 }
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NexusCart.Repository.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NexusCart.Models
 {
@@ -6,17 +8,32 @@ namespace NexusCart.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm!")]
         public string ?Name { get; set; }
+
         public string ?Slug { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+
+        [Required(ErrorMessage = "Vui lòng nhập mô tả sản phẩm!")]
         public string ?Description { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Price must be at least 4 characters long.")]
+
+        [Required(ErrorMessage = "Vui lòng nhập giá sản phẩm lớn hơn 0!")]
+        [Range(0.01, double.MaxValue)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn thương hiệu!"), Range(1, int.MaxValue)]
         public int BrandId { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn danh mục!"), Range(1, int.MaxValue)]
         public int CategoryId { get; set; }
+
         public CategoryModel ?Category { get; set; }
+
         public BrandModel ?Brand { get; set; }
+
         public string ?ImageUrl { get; set; }
+
+        [NotMapped]
+        [File]
+        public IFormFile ?ImageFile { get; set; }
     }
 }
