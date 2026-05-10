70b9d017b5c3914d5879a286be5fcd7a65893e30
datweb07 <dat82770@gmail.com>
2026-05-10 11:13:41 +0700

    feat: add validation for uploaded files

---
+++ Repository/Validation/FileAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusCart.Repository.Validation
+{
+    public class FileAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                //kiểm tra xem file có phải là hình ảnh hay không
+                //if (file.ContentType.StartsWith("image/"))
+                //{
+                //    return ValidationResult.Success;
+                //}
+                //else
+                //{
+                //    return new ValidationResult("Only image files are allowed.");
+                //}
+
+                var extension = Path.GetExtension(file.FileName).ToLower();
+                string [] allowedExtensions = { ".jpg", ".jpeg", ".png", ".gif" };
+
+                bool result = allowedExtensions.Any(x => extension.EndsWith(x));
+                if (!result)
+                {
+                    return new ValidationResult("Only image files are allowed.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
