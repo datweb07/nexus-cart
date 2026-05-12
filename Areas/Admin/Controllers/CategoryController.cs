6e292402e5f44ac98d82768abc8b2fb0981ee248
Đạt Trương <dat82770@gmail.com>
2026-05-12 22:14:20 +0700

    feat: implement product category page (#27)

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NexusCart.Repository;
+
+namespace NexusCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly DBContext _context;
+        public CategoryController (DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
