170d5ff41257d9f638af130a683b984c565728e6
datweb07 <dat82770@gmail.com>
2026-05-13 15:58:00 +0700

    feat: implement product category creation

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NexusCart.Models;
 using NexusCart.Repository;
 
 namespace NexusCart.Areas.Admin.Controllers
@@ -15,6 +17,47 @@ namespace NexusCart.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Categories.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryModel.Slug = categoryModel.Name?.ToLower().Replace(" ", "-");
+                var slug = await _context.Categories.Where(p => p.Slug == categoryModel.Slug).FirstOrDefaultAsync();
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("Name", "A category with the same name already exists.");
+                    return View(categoryModel);
+                }
+                
+                _context.Add(categoryModel);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Category creation failed";
+                List<String> errors = new List<String>();
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("; ", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(categoryModel);
         }
     }
 }
--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -6,11 +6,10 @@ namespace NexusCart.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        [Required(ErrorMessage = "Name must be at least 4 characters long.")]
         public string ?Name { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+        [Required(ErrorMessage = "Description must be at least 4 characters long.")]
         public string ?Description { get; set; }
-        [Required]
         public string ?Slug { get; set; }
         public int Status { get; set; }
     }
