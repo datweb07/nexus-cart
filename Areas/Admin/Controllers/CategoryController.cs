48bcb0a9a6815c465430e5d762ddc601721a7ba4
Đạt Trương <dat82770@gmail.com>
2026-05-14 23:25:03 +0700

    feat: implement product category update functionality (#29)

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -59,5 +59,67 @@ namespace NexusCart.Areas.Admin.Controllers
             }
             return View(categoryModel);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        { 
+            CategoryModel category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryModel.Slug = categoryModel.Name?.ToLower().Replace(" ", "-");
+                var slug = await _context.Categories.Where(p => p.Slug == categoryModel.Slug).FirstOrDefaultAsync();
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("Name", "A category with the same name already exists.");
+                    return View(categoryModel);
+                }
+
+                _context.Update(categoryModel);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Category update failed";
+                List<String> errors = new List<String>();
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("; ", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(categoryModel);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            CategoryModel category = await _context.Categories.FindAsync(id);
+            
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
