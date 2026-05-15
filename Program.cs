1f5067235ae04772701c1c3558ff55bbd5461c32
Đạt Trương <dat82770@gmail.com>
2026-05-15 14:25:41 +0700

    feat: configure custom route for index page (#32)

--- Program.cs
+++ Program.cs
@@ -60,6 +60,16 @@ namespace NexusCart
                 pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
+                name: "category",
+                pattern: "/category/{Slug?}",
+                defaults: new { controller = "Category", action = "Index" });
+
+            app.MapControllerRoute(
+                name: "brand",
+                pattern: "/brand/{Slug?}",
+                defaults: new { controller = "Brand", action = "Index" });
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
