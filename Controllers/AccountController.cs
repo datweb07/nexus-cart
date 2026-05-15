0d3e7ca46e52be04948d85d036648e2a218ee5d6
Đạt Trương <dat82770@gmail.com>
2026-05-15 22:01:29 +0700

    feat: implement account pages and account controller (#36)

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NexusCart.Models;
+
+namespace NexusCart.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUserModel> _userManager;
+        private SignInManager<AppUserModel> _signInManager;
+        private AccountController(UserManager<AppUserModel> userManager, SignInManager<AppUserModel> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusCart.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên người dùng!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email!"), EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required(ErrorMessage = "Vui lòng nhập mật khẩu!")]
+        public string Password { get; set; }
+    }
+}
