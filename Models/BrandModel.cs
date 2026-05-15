2570c7ba5befcb9fdcf26a12bf8af4b251cd5b63
Đạt Trương <dat82770@gmail.com>
2026-05-15 13:56:43 +0700

    feat: implement product brand management (#30)

--- Models/BrandModel.cs
+++ Models/BrandModel.cs
@@ -6,9 +6,9 @@ namespace NexusCart.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+        [Required(ErrorMessage = "Vui lòng nhập tên thương hiệu")]
         public string ?Name { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Description must be at least 4 characters long.")]
+        [Required(ErrorMessage = "Vui lòng nhập mô tả thương hiệu")]
         public string ?Description { get; set; }
         public string ?Slug { get; set; }
         public int Status { get; set; }
