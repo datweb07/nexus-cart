45d78f15d339f12fac6a31555e8927405233db65
Đạt Trương <dat82770@gmail.com>
2026-05-18 21:40:56 +0700

    feat: implement order creation functionality (#42)

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexusCart.Models;
+using NexusCart.Repository;
+using System.Security.Claims;
+
+namespace NexusCart.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly DBContext _context;
+        public CheckoutController (DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            if (userEmail == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                var oderCode = Guid.NewGuid().ToString();
+                var order = new OrderModel();
+                order.OrderCode = oderCode;
+                order.UserName = userEmail;
+                order.OrderStatus = 1;
+                order.CreatedDate = DateTime.Now;
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+
+                TempData["success"] = "Order placed successfully!";
+                return RedirectToAction("Index", "Cart");
+            }
+            return View();
+        }
+    }
+}
