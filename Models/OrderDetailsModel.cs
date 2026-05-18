4df0845c1ad2a536a9b55b0cfb34bcccb914ca7c
datweb07 <dat82770@gmail.com>
2026-05-18 21:56:41 +0700

    feat: implement order details functionality

--- Models/OrderDetailsModel.cs
+++ Models/OrderDetailsModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public int OrderCode { get; set; }
+        public string OrderCode { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
