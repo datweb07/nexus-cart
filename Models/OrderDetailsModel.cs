19f82dcec58d0f8215750ff63da51436f21455a2
Đạt Trương <dat82770@gmail.com>
2026-05-19 10:00:40 +0700

    feat: implement admin order detail view (#45)

--- Models/OrderDetailsModel.cs
+++ Models/OrderDetailsModel.cs
@@ -1,4 +1,6 @@
-﻿namespace NexusCart.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NexusCart.Models
 {
     public class OrderDetailsModel
     {
@@ -8,5 +10,7 @@
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
+        [ForeignKey("ProductId")]
+        public ProductModel Product { get; set; }
     }
 }
