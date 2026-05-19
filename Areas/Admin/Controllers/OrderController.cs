6a8eb7f4c1b88b7298c2b24a030721e94d42ee7f
datweb07 <dat82770@gmail.com>
2026-05-19 09:38:15 +0700

    feat: implement admin order listing page

---
+++ Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NexusCart.Repository;
+
+namespace NexusCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly DBContext _context;
+        public OrderController(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
